1d7ae28a45e3cab14c875aba58212f62d0e00ffd
EsGarciaC <esgarciac00@gmail.com>
2024-02-09 17:44:07 -0500

    Done

--- pregunta.py
+++ pregunta.py
@@ -10,11 +10,29 @@ import pandas as pd
 
 
 def clean_data():
-
     df = pd.read_csv("solicitudes_credito.csv", sep=";")
 
-    #
-    # Inserte su código aquí
-    #
+    df.drop("Unnamed: 0", axis="columns", inplace=True)
+
+    # limpieza de monto de credito, signo, espacios, comas y pasar de float a int por los puntos flotantes
+    df["monto_del_credito"] = df["monto_del_credito"].apply(lambda x: int(float(x.replace("$", "")
+                                                            .strip()
+                                                            .replace(",", "")
+                                                            )))
+    # Las fechas varían de formato, hay que poner dayfirst y format mixed
+    df["fecha_de_beneficio"] = pd.to_datetime(df["fecha_de_beneficio"], dayfirst=True, format='mixed')
+
+    for column in df.columns:
+        # no funciona bien if isinstance(df[column][0], str) por algún motivo.
+        df[column] = df[column].apply(lambda stri:
+                                      stri.lower()
+                                      .replace("-", " ")
+                                      .replace("_", " ")
+                                      if isinstance(stri, str) else stri
+                                      )
+
+    # Si se ponen al inicio no funciona
+    df.dropna(axis=0, inplace=True)
+    df.drop_duplicates(inplace=True)
 
     return df
